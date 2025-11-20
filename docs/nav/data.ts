035e3e37f22c61727e49ae980cab99b1f823c129
jonaslgtm <10245089@qq.com>
2025-11-20 10:28:39 +0800

    update nav

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -29,36 +29,6 @@ export const NAV_DATA: NavData[] = [
         title: '微信公众平台',
         desc: '管理服务号、公众号、小程序',
         link: 'https://mp.weixin.qq.com/',
-      },
-      {
-        icon: 'https://devtool.tech/logo.svg',
-        title: '开发者武器库',
-        desc: '开发者武器库，做开发者最专业最好用的专业工具箱',
-        link: 'https://devtool.tech'
-      },
-      {
-        icon: 'https://tool.lu/favicon.ico',
-        title: '在线工具',
-        desc: '开发人员的工具箱',
-        link: 'https://tool.lu'
-      },
-      {
-        icon: '/icons/json-cn.ico',
-        title: 'Json 中文网',
-        desc: 'JSON 在线解析及格式化验证',
-        link: 'https://www.json.cn'
-      },
-      {
-        icon: 'https://transform.tools/static/favicon.png',
-        title: 'transform',
-        desc: '一个支持多语言的在线转换器',
-        link: 'https://transform.tools/json-schema-to-typescript'
-      },
-      {
-        icon: '/icons/excalidraw.ico',
-        title: 'Excalidraw',
-        desc: '手绘风格流程图',
-        link: 'https://excalidraw.com'
       }
     ]
   },
